bed4d9059eec2b837678a3a59a15f226877c2cc2
Quinn C. Hoyer <quinn@themightyquinn.com>
2014-08-11 20:33:21 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function textBot() {
+	"use strict";
+
+	var Bot = require('tmq-twitter-bot'),
+		tweets = require('./tweets'),
+		_ = require('underscore'),
+		config;
+
+	try {
+		config = require('./config');
+	} catch (err) {
+		config = {
+			consumer_key: process.env.CONSUMER_KEY,
+			consumer_secret: process.env.CONSUMER_SECRET,
+			access_token: process.env.ACCESS_TOKEN,
+			access_token_secret: process.env.ACCESS_TOKEN_SECRET
+		};
+	}
+
+	var bot = new Bot(config);
+
+	var status;
+
+	while (!status || status.length > 140) {
+		status = tweets[_.random(tweets.length - 1)];
+	}
+
+	bot.tweet(status)
+		.then(function (result) {
+			console.log(result);
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+}
+
+setInterval(textBot, 1 * 60 * 1000); //One minute
---
+++ tweets.js
@@ -0,0 +1,19 @@
+module.exports = [
+	"Organic Neutra Thundercats, occupy wolf shabby chic synth PBR tote bag Cosby sweater slow-carb lo-fi pour-over. Pop-up crucifix fanny pack, Schlitz bitters squid forage.",
+
+	"Mixtape flexitarian literally, deep v forage retro quinoa DIY narwhal plaid Bushwick 90's wayfarers. Quinoa post-ironic Echo Park meh, flannel skateboard crucifix single-origin coffee iPhone.",
+
+	"Portland ennui shabby chic, bitters kogi letterpress tote bag. Helvetica sustainable Carles fixie. VHS typewriter tattooed +1 street art cred.",
+
+	"Asymmetrical Austin occupy Wes Anderson, flexitarian hashtag Truffaut cray church-key.",
+
+	"Fashion axe raw denim cliche kitsch tattooed, post-ironic you probably haven't heard of them selfies retro lomo butcher synth next level. Meggings shabby chic food truck, fixie Kickstarter Banksy next level pug pork belly. Cliche leggings messenger bag beard skateboard.",
+
+	"Disrupt direct trade Helvetica butcher. Before they sold out Blue Bottle scenester literally, Austin Thundercats butcher Pitchfork pug cornhole sustainable cardigan wolf. Selfies kogi stumptown crucifix, Blue Bottle pour-over gluten-free flexitarian.",
+
+	"Gentrify Odd Future wayfarers +1 selfies, fingerstache butcher raw denim flexitarian Truffaut lo-fi High Life before they sold out fap.",
+
+	"Fap Godard tote bag, DIY photo booth ugh craft beer fixie deep v. Fap dreamcatcher normcore, organic shabby chic YOLO cardigan post-ironic pug.",
+
+	"Wes Anderson crucifix fingerstache, semiotics mustache kitsch shabby chic forage squid PBR umami. Kogi tote bag plaid, master cleanse semiotics tattooed mumblecore whatever squid PBR&B hashtag next level kale chips ethical. Quinoa shabby chic Echo Park cred, Carles stumptown post-ironic chia put a bird on it lomo keytar vinyl Cosby sweater mustache. IPhone freegan Tumblr tousled pickled forage."
+];
